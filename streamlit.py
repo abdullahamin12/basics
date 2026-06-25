d92d0ea9291ff340b0e4574a7b0931963c26e504
abdullahamin12 <abdullahamin12570@gmail.com>
2026-06-26 01:44:12 +0500

    feat:adding check box on a frontend

--- streamlit.py
+++ streamlit.py
@@ -3,6 +3,15 @@ import requests
 URL="http://127.0.0.1:8000"
 st.title("Todo App")
 st.write("Connected to fastapi backend")
+
+with   st.form("Todo form "):
+    title=st.text_input("Add todo title")
+    done=st.checkbox("done")
+    submited=st.form_submit_button("add todo")
+    if submited:
+        st.write("TItle:",title)
+        st.write("DONE:",done)
+
 responce=requests.get(f"{URL}/todos/")
 if responce.status_code==200:
     todos=responce.json()
