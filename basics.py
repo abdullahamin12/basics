9b7c8b5bf23b8e07d171aae487b4a943cc350788
abdullahamin12 <abdullahamin12570@gmail.com>
2026-06-25 17:53:17 +0500

    feat:adding delete method

--- basics.py
+++ basics.py
@@ -8,9 +8,27 @@ todos=[
     {"id": 1, "title": "Learn FastAPI", "done": False},
     {"id": 2, "title": "Practice CRUD", "done": True}
 ]
-todo = Todo(title="Buy milk", done=False)
-print( todo.model_dump() )
+@app.get("/todos/")
+def show():
+    return todos
+@app.get("/todos/{item_id}")
+def show_id(item_id:int):
+    for todo in todos:
+        if item_id==todo["id"]:
+            return todo
+        
+    return {"error":"item did not found"}
+@app.post("/todos/")
+def add(todo:Todo):
+    new_todo={"id":len(todos)+1,**todo.model_dump()}
+    todos.append(new_todo)
+    return(new_todo)
+@app.delete("/todos/{item_id}")
 
-
-
-
+def remove(item_id: int):
+    for todo in todos:
+        if item_id == todo["id"]:
+            todos.remove(todo)
+            return {"done": "deleted permanently"}
+    return {"not found": "item not found"}
+            
