410f5c8560b5f3c8188fc768ebee1c1a3f9810e1
abdullahamin12 <abdullahamin12570@gmail.com>
2026-06-26 12:53:56 +0500

    feature:adding data frame instead of raw json

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 import requests
+import pandas as pd
 URL="http://127.0.0.1:8000"
 st.title("Todo App")
 st.write("Connected to fastapi backend")
@@ -40,8 +41,8 @@ responce=requests.get(f"{URL}/todos/")
 if responce.status_code==200:
     todos=responce.json()
     st.subheader("All todos")
-    st.json(todos)
-    
+    df = pd.DataFrame(todos)
+    st.dataframe(df)
 else:
     print("error")
 
